056e97df6d36c3cba11b834127a0522dfa4a4633
Eahiya <eahiamondal@gmail.com>
2020-08-20 19:19:09 +0600

    vue templating done and install vue-router and setup

---
+++ resources/js/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="Main">
+        <div id="wrapper">
+            <!-- Sidebar -->
+            <sidebar></sidebar>
+            <!-- Sidebar -->
+            <div id="content-wrapper" class="d-flex flex-column">
+                <div id="content">
+                    <!-- TopBar -->
+                    <top-bar></top-bar>
+                    <!-- Topbar -->
+                    <!-- Container Fluid-->
+                    <div class="container-fluid" id="container-wrapper">
+                        <router-view></router-view>
+                    </div>
+                    <!---Container Fluid-->
+                </div>
+                <!-- Footer -->
+                    <footer></footer>
+                <!-- Footer -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import Sidebar from "./components/Sidebar";
+import Footer from "./components/Footer";
+import TopBar from "./components/TopBar";
+export default {
+    name: "App",
+    components: {TopBar, Footer, Sidebar}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/js/components/ScrollToTop.vue
@@ -0,0 +1,16 @@
+<template>
+    <!-- Scroll to top -->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+</template>
+
+<script>
+export default {
+    name: "ScrollToTop"
+}
+</script>
+
+<style scoped>
+
+</style>
